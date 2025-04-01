1564b127aa3445b0afcf79e3702fa8a0bbf100a3
agamrossen <agamrossen06@gmail.com>
2025-04-01 17:20:54 +0100

    Initial Commit

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include "mbed.h"
+#include "joystick.cpp"
+#include "buttons.cpp"
+#include "display.cpp"
+#include "player.cpp"
+#include "patterns.cpp"
+#include <cstdio>
+#include <limits>
+#include <stdio.h>
+#include <array>
+
+int main() {
+    
+    while (true) { }
+}
