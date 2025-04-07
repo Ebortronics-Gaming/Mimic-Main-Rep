180448ff2b4b28507c6673f5624d02b517356f82
agamrossen <agamrossen06@gmail.com>
2025-04-07 14:10:42 +0100

    Update main.cpp
    Incorporates button and player class to perform foundational functionalities

--- main.cpp
+++ main.cpp
@@ -9,7 +9,36 @@
 #include <stdio.h>
 #include <array>
 
+Player player;
+
+void btn1Action();
+void btn2Action();
+void btn3Action();
+
+// Button objects with different pins & callbacks
+Button btn1(D12, btn1Action);    // level changing button
+Button btn2(D11, btn2Action);
+Button btn3(D10, btn3Action);
+
+
 int main() {
-    
+
     while (true) { }
+}
+
+
+// Button Action Functions:
+void btn1Action() {     // increases level
+    printf("Button 1 is pressed\n");
+    player.NextLevel();
+}
+
+void btn2Action() {
+    printf("Button 2 is pressed\n");
+    // to be continued...
+}
+
+void btn3Action() {
+    printf("Button 3 is pressed\n");
+    // to be continued...
 }
