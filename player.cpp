547e27352db1b782e2666c9350c0c84631b5f117
agamrossen <agamrossen06@gmail.com>
2025-04-08 12:12:45 +0100

    Update player.cpp
    with the new player ideas and their working functions of: hearts, number of arrows and monsters, and countdown

--- player.cpp
+++ player.cpp
@@ -6,6 +6,10 @@ public:
     static int score; // global variables that are all static
     static int level;
     static int flashTime;
+    static int arrowNum;
+    static int monsterNum;
+    static int countdown;   // in seconds (600s = 10 mins)
+    static int hearts;
 
     
     static void addScore(int points) {  // adds the given points to the current score
@@ -17,21 +21,30 @@ public:
 
         if (level == 1) {
             flashTime = 800;
+            arrowNum = 4;
+            monsterNum = 4;
         } else if (level == 2) {
             flashTime = 600;
+            arrowNum = 5;
+            monsterNum = 5;
         } else if (level == 3) {
             flashTime = 400;
+            arrowNum = 6;
+            monsterNum = 6;
         } else if (level == 4) {
             flashTime = 200;
+            arrowNum = 7;
+            monsterNum = 7;
         }
     }
 
-    static void NextLevel() {
+    static void nextLevel() {
         int newLevel; 
         if (level != 4) {
             int newLevel = level + 1;
         } else {
             int newLevel = 1;
+            gameOver(1);
         }
         setLevel(newLevel);
     }
@@ -39,8 +52,42 @@ public:
     static void ResetLevel() {
         setLevel(1);
     }
+
+    static void minusMonster() {
+        if ((monsterNum = 1) && (level = 4)) {
+            gameOver(1);
+        } else if ((monsterNum = 1) && (level != 4)) {
+            nextLevel();
+        } else if (monsterNum != 1) {
+            monsterNum -= 1;
+        }
+    }
+
+    static void gameOver(int i) {
+        ResetLevel();
+        score = 0;
+        hearts = 3;
+        if (i) {
+            printf("You win!!");
+        } else {
+            printf("You lose!!");
+        }
+    }
+
+    static void minusHearts() {
+        if (hearts != 1) {
+            hearts -= 1;
+        } else {
+            gameOver(0);
+        }
+    }
+
 };
 
 int Player::score = 0;  // initialize static members with initial values
 int Player::level = 1;
 int Player::flashTime = 800;
+int Player::arrowNum = 4;
+int Player::countdown = 600;
+int Player::monsterNum = 4;
+int Player::hearts = 3;
