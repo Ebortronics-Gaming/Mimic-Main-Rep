39e50e5619393fda70fc8ec4522b019040718348
vishwawarnajith <lpp527@york.ac.uk>
2025-04-09 23:27:39 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,7 +23,252 @@ Button btn3(D10, btn3Action);
 
 int main() {
 
+    
+    /*==============================Level 1==============================*/
+
+    if(level == 1)
+    {
+        display.welcome(); 
+        thread_sleep_for(2000);
+        lcd.cls();
+        display.defaultDisplay();
+        timer.updateDisplay(8);
+
+        int i = 0;
+        int score = 0;
+
+        while(exit != 1 && 15-(i+1) != 1 && timer.isFinished() != 1)
+        {
+            generator.pattern(4); 
+            int* seq = generator.getSequence();
+            int* user = joystick.pattern(4);
+
+            bool arraysAreEqual(int a[], int b[]) 
+            {
+                for (int j = 0; j < 4; j++) 
+                {
+                    if (a[j] != b[j]) 
+                    {
+                        return false; 
+                    }
+                }
+                return true;
+            }
+
+            if (arraysAreEqual(seq, user)) 
+            {
+                check = 2;
+                score += 1;
+                display.scoreUpdate();
+                lcd.locate(13,0);
+                lcd.printf("%d", score);
+
+            } 
+            else 
+            {
+                check = 1;
+            }
+
+            if(check == 1)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                ++i;
+                check = 0;
+            }
+            else if(check == 2)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                display.displayChar(10, 15-(i+1), 1);
+                check = 0;
+            }
+        }
+
+        if(timer.isFinished() == 1)
+        {
+            showWinScreen(lcd, score);
+        }
+        if(15-(i+1) == 1)
+        {
+            showLoseScreen(lcd, score);
+        }
+    }
+
+    /*==============================Level 2==============================*/ 
+
+    if(level == 2)
+    {
+        display.welcome();
+        thread_sleep_for(2000);
+        lcd.cls();
+        display.defaultDisplay();
+        timer.updateDisplay(6);
+
+        int i = 0;
+        int score = 0;
+
+        while(exit != 1 && 15-(i+1) != 1 && timer.isFinished() != 1)
+        {
+            generator.pattern(6); 
+            int* seq = generator.getSequence();
+            int* user = joystick.pattern(6);
+
+            bool arraysAreEqual(int a[], int b[]) 
+            {
+                for (int j = 0; j < 6; j++) 
+                {
+                    if (a[j] != b[j]) 
+                    {
+                        return false; 
+                    }
+                }
+                return true;
+            }
+
+            if (arraysAreEqual(seq, user)) 
+            {
+                check = 2;
+                score += 1;
+                display.scoreUpdate();
+                lcd.locate(13,0);
+                lcd.printf("%d", score);
+            } 
+            else 
+            {
+                check = 1;
+            }
+
+            if(check == 1)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                ++i;
+                check = 0;
+            }
+            else if(check == 2)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                display.displayChar(10, 15-(i+1), 1);
+                check = 0;
+            }
+        }
+        
+        if(timer.isFinished() == 1)
+        {
+            showWinScreen(lcd, score);
+        }
+        if(15-(i+1) == 1)
+        {
+            showLoseScreen(lcd, score);
+        }
+    }
+
+    /*==============================Level 3==============================*/
+
+    if(level == 3)
+    {
+        display.welcome();
+        thread_sleep_for(2000);
+        lcd.cls();
+        display.defaultDisplay();
+        timer.updateDisplay(8);
+
+        int i = 0;
+        int score = 0;
+
+        while(exit != 1 && 15-(i+1) != 1 && timer.isFinished() != 1)
+        {
+            generator.pattern(8); 
+            int* seq = generator.getSequence();
+            int* user = joystick.pattern(8);
+
+            bool arraysAreEqual(int a[], int b[]) 
+            {
+                for (int j = 0; j < 8; j++) 
+                {
+                    if (a[j] != b[j]) 
+                    {
+                        return false; 
+                    }
+                }
+                return true;
+            }
+
+            if (arraysAreEqual(seq, user)) 
+            {
+                check = 2;
+                score += 1;
+                display.scoreUpdate();
+                lcd.locate(13,0);
+                lcd.printf("%d", score);
+            } 
+            else 
+            {
+                check = 1;
+            }
+
+            if(check == 1)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                ++i;
+                check = 0;
+            }
+            else if(check == 2)
+            {
+                display.displayChar(3, 15-(i+1), 1);
+                thread_sleep_for(500);
+                display.displayChar(10, 15-(i+1), 1);
+                check = 0;
+            }
+        }
+
+        if(timer.isFinished() == 1)
+        {
+            showWinScreen(lcd, score);
+        }
+        if(15-(i+1) == 1)
+        {
+            showLoseScreen(lcd, score);
+        }
+    }
+
     while (true) { }
+    
+}
+
+void showLoseScreen(TextLCD &lcd, int score) {
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("   YOU LOOSED!   ");
+    lcd.locate(0, 1);
+    lcd.printf("Your Score : %d", score);
+    thread_sleep_for(2000);
+
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("  Take a Break  ");
+    lcd.locate(0, 1);
+    lcd.printf("Try Again Later!");
+    thread_sleep_for(2000);
+}
+
+void showWinScreen(TextLCD &lcd, int score) {
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("    YOU WON!   ");
+    lcd.locate(0,1);
+    lcd.printf("Your Score : %d", score);
+    thread_sleep_for(2000);
+    
+    lcd.cls();
+    lcd.locate(0, 0);
+    lcd.printf("   Well Done!   ");
+    lcd.locate(0,1);
+    lcd.printf(" Rest Your Eyes ");
+    thread_sleep_for(2000);
 }
 
 
