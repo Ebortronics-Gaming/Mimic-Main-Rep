aa6d467f4c837d021468bfef91951864d26e1e8c
vishwawarnajith <lpp527@york.ac.uk>
2025-04-09 23:33:09 +0100

    Create TimerDisplay.h

---
+++ TimerDisplay.h
@@ -0,0 +1,25 @@
+#ifndef TIMERDISPLAY_H
+#define TIMERDISPLAY_H
+
+#include "mbed.h"
+#include "TextLCD_CC.h"
+
+class TimerDisplay {
+private:
+    TextLCD &lcd;
+    int minutes;
+    int seconds;
+    bool timerRunning;
+    Thread timerThread;
+    int finishedFlag;
+
+    // Internal method run on a separate thread
+    void countdownLoop();
+
+public:
+    TimerDisplay(TextLCD &lcdRef);
+    void updateDisplay(int startMinutes); // Starts the countdown
+    int isFinished();
+};
+
+#endif
