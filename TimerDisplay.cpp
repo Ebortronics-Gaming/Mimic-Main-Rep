c7e06616f8dd4bdab78655d369542af175420f01
vishwawarnajith <lpp527@york.ac.uk>
2025-04-09 23:33:58 +0100

    Create TimerDisplay.cpp

---
+++ TimerDisplay.cpp
@@ -0,0 +1,44 @@
+#include "TimerDisplay.h"
+
+TimerDisplay::TimerDisplay(TextLCD &lcdRef)
+    : lcd(lcdRef), minutes(0), seconds(0), timerRunning(false), finishedFlag(0) {}
+
+void TimerDisplay::updateDisplay(int startMinutes) {
+    minutes = startMinutes - 1;
+    seconds = 59;
+    finishedFlag = 0;
+    timerRunning = true;
+
+    timerThread.start(callback(this, &TimerDisplay::countdownLoop));
+}
+
+void TimerDisplay::countdownLoop() {
+    while (timerRunning) {
+        lcd.locate(7, 0);
+        lcd.printf("%01d:%02d", minutes, seconds);
+
+        if (minutes == 0 && seconds == 0) 
+        {
+            timerRunning = false;
+            finishedFlag = 1;
+            lcd.cls();
+            return;
+        }
+
+        if (seconds == 0) 
+        {
+            minutes--;
+            seconds = 59;
+        } 
+        else 
+        {
+            seconds--;
+        }
+
+        ThisThread::sleep_for(1000ms);
+    }
+}
+
+int TimerDisplay::isFinished() {
+        return finishedFlag;
+    }
