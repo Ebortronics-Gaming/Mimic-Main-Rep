69efde9fa334df29c78e1a05e4a6d05f37c13885
vishwawarnajith <lpp527@york.ac.uk>
2025-04-09 23:29:32 +0100

    Create PatternGenerator.cpp

---
+++ PatternGenerator.cpp
@@ -0,0 +1,30 @@
+#include "PatternGenerator.h"
+#include <cstdlib>
+#include <ctime>
+
+PatternGenerator::PatternGenerator(CustomCharDisplay &dispRef) : display(dispRef), sequenceSize(0) {}
+
+void PatternGenerator::pattern(int y) {
+    sequenceSize = y;   
+
+    for (int i = 0; i < y; i++) {
+        srand(time(NULL));
+        int num = (rand() % 4) + 6;   // Generate random number between 6–9
+        sequence[i] = num;
+        display.displayChar(num, 2, 0);
+        thread_sleep_for(num*100);
+        display.displayChar(10,2,0);
+        thread_sleep_for(num*100);
+    }
+}
+
+int* PatternGenerator::getSequence() {
+    /*for (int i = 0; i < sequenceSize; i++) {
+        printf("%d ", sequence[i]);
+    }*/
+    return sequence;
+}
+
+int PatternGenerator::getSequenceSize() {
+    return sequenceSize;
+}
