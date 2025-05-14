a4da594e605aa2cf9aba2f73a248852bf124752d
agamrossen <agamrossen06@gmail.com>
2025-05-14 15:15:09 +0100

    Update player.h

--- player.h
+++ player.h
@@ -1,1 +1,20 @@
+#ifndef PLAYER_H
+#define PLAYER_H
 
+#include <stdint.h>
+
+class Player {
+public:
+    static int score;
+    static int level;
+    static int arrowNum;
+    static int monsterNum;
+
+    static void reset();
+    static void setLevel(int lvl);
+    static void addScore(int pts = 1);
+    static void addMonster();
+    static bool isGameOver();
+};
+
+#endif // PLAYER_H
