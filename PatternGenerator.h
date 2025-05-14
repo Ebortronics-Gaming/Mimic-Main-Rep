e5995882a6cc9545dfbb300fddedc21937ee0af8
agamrossen <agamrossen06@gmail.com>
2025-05-14 15:10:59 +0100

    Update PatternGenerator.h

--- PatternGenerator.h
+++ PatternGenerator.h
@@ -1,24 +1,19 @@
-// -----------------------------------------------------------------------------------
-// Developed by Vishwa (Do not modify without prior discussion)
-// Last Modified : 09 April 2025 by author 
-// Please consult the original author before making any changes to ensure consistency
-// -----------------------------------------------------------------------------------
-
 #ifndef PATTERN_GENERATOR_H
 #define PATTERN_GENERATOR_H
 
 #include "mbed.h"
 #include "TextLCD_CC.h"
-#include "CustomCharDisplay.h"
+
+class display; // Forward declaration
 
 class PatternGenerator {
 private:
-    CustomCharDisplay &display;
+    display &disp;
     int sequence[10];    
     int sequenceSize;
 
 public:
-    PatternGenerator(CustomCharDisplay &displayRef);
+    PatternGenerator(display &dispRef);
     void pattern(int y);         
     int* getSequence();             
     int getSequenceSize();           
