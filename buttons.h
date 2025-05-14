832f5a8111b7012c5bbc6267ee5f4cb8dcac5268
agamrossen <agamrossen06@gmail.com>
2025-05-14 15:12:59 +0100

    Update buttons.h

--- buttons.h
+++ buttons.h
@@ -1,1 +1,19 @@
+#ifndef BUTTONS_H
+#define BUTTONS_H
 
+#include "mbed.h"
+
+class Button {
+public:
+    Button(PinName pin, void (*cb)());
+    void init();
+    void clearInterrupt();
+    void debounce();
+private:
+    InterruptIn btn;
+    Semaphore sem;
+    Thread thread;
+    void (*callback)();
+};
+
+#endif // BUTTONS_H
